40d1b590820850a92c4723736b98e3617d35d384
birukyemane <bir_yem@yahoo.com>
2019-01-16 12:12:27 +0200

    feat. - fetched all stations from Virta's API and show the on the map.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import MapContainer from './components/MapContainer';
 import './App.css';
 
+import MapContainer from './components/MapContainer';
+import axios from 'axios';
+
+
 class App extends Component {
+  state = {
+    stations:[]
+  };
+
+  componentDidMount () {  
+      axios.get('https://api.virta.fi/v4/stations').then((response) => {
+          this.setState({
+              stations:response.data
+          }) // error handling ? 
+      })
+  }
   render() {
     return (
       <div className="App">
-         <MapContainer />
+        <div>
+          <h1>Virta stations</h1>
+        </div>
+         <MapContainer stations={this.state.stations}/>
       </div>
     );
   }
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -12,6 +12,7 @@ export class MapContainer extends Component {
     selectedPlace: {},
   };
  
+ 
   onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
@@ -29,6 +30,11 @@ export class MapContainer extends Component {
   };
  
   render() {
+    const stationMarkers = this.props.stations.map((station,index) =>{
+      return <Marker key={index+1} onClick={this.onMarkerClick}
+                name={station.name}
+                position={{lat: station.latitude, lng: station.longitude}} />
+    });
     return (
       <Map google={this.props.google}
           onClick={this.onMapClicked}>
@@ -37,6 +43,7 @@ export class MapContainer extends Component {
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} 
                 position={{lat: 37.778519, lng: -122.405640}} />
+        {stationMarkers}
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}>
