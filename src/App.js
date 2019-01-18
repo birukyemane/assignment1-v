004535d8ef3cda6c95ba496e37d8a583311d772d
birukyemane <bir_yem@yahoo.com>
2019-01-18 11:41:31 +0200

    Feature- show status on map. differnt icons used based on the status of a station. however, since how to determine the status was not clear. I used only the status of the first element in the 'evses' object to determine the status. Issues- marker clusters might hlep when there are a lot of markers in one area, but couldnt find from documentation for now. Feature- current location implemente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import MapContainer from './components/MapContainer';
 import axios from 'axios';
+import MapContainer from './components/VirtaMap';
+
 
 class App extends Component {
   state = {
@@ -10,15 +11,37 @@ class App extends Component {
   };
 
   componentDidMount () {  
+      let stations = [];
+      let stationsWithStatus = [];
       axios.get('https://api.virta.fi/v4/stations').then((response) => {
-          this.setState({
-              stations:response.data
-          }) // error handling ? 
-      })
+         stations = response.data;
+         axios.get('https://api.virta.fi/v4/stations/status').then((response) => {
+            stationsWithStatus = response.data;
+            stations = stations.map(station=>{
+              let result = stationsWithStatus.filter((stationWithStatus)=>{
+                return station.id === stationWithStatus.id; 
+              })
+              if(result.length >0) {               
+                station.status = result[0].evses[0].status; // determine the status based on only the status of the first element of the evses.  
+              } else {
+                station.status = 0;
+              }
+            return station;
+            })    
+            this.setState({
+              stations: stations
+            })
+         }).catch(function (error) {
+              console.log('stations status error',error);
+         }) // error   
+      }).catch(function (error) {
+          console.log('stations error', error);
+      })   // error     
   }
+
   render() {
     return (
-      <div className="App">       
+      <div className="App">             
         <MapContainer stations={this.state.stations}/>
       </div>
     );
